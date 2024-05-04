efbd4b39deaa467790d61772d7679c6a6b9d4ff7
Pilly27 <146407229+Pilly27@users.noreply.github.com>
2024-05-04 09:31:08 -0500

    Librerias

---
+++ src/main/java/com/upc/jpa_chambav1/JpaChambaV1Application.java
@@ -0,0 +1,13 @@
+package com.upc.jpa_chambav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaChambaV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaChambaV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/upc/jpa_chambav1/JpaChambaV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.jpa_chambav1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaChambaV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
